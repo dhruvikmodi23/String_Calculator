00a681c18811dee4e0cadfd35ccaab92060f453b
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2025-07-04 00:00:31 +0530

    return number for single number input

--- Test/stringCalculator.test.js
+++ Test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("../stringCalculator");
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number for single number string', () => {
+    expect(add("4")).toBe(4);
+  });
