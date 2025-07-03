a5012ee3866bd395730aa4e2fde5d2a44e11d2e9
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2025-07-04 00:10:09 +0530

    Add test for two numbers which is seperated by comma

--- Test/stringCalculator.test.js
+++ Test/stringCalculator.test.js
@@ -1,9 +1,16 @@
 const add = require("../stringCalculator");
 
+//Add test for empty string
 test("returns 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
 
+//Add test for single number
 test('returns number for single number string', () => {
     expect(add("4")).toBe(4);
+  });
+
+  //Add test for two numbers which is separated with comma
+  test('returns the sum of two comma-separated numbers', () => {
+    expect(add('1,5')).toBe(6);
   });
