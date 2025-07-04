570034bf07dde481db68a77747954d884e70a914
dhruvikmodi23 <dhruvikmodi23@gmail.com>
2025-07-04 10:15:24 +0530

    Implement negetive number checking

--- Test/stringCalculator.test.js
+++ Test/stringCalculator.test.js
@@ -6,30 +6,32 @@ test("returns 0 for empty string", () => {
 });
 
 //Add test for single number
-test('returns number for single number string', () => {
-    expect(add("4")).toBe(4);
-  });
+test("returns number for single number string", () => {
+  expect(add("4")).toBe(4);
+});
 
-  //Add test for two numbers which is separated with comma
-  test('returns the sum of two comma-separated numbers', () => {
-    expect(add('1,5')).toBe(6);
-  });
+//Add test for two numbers which is separated with comma
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,5")).toBe(6);
+});
 
-  //Add test for unknown amount of numbers
-  test('returns the sum of any amount of numbers', () => {
-    expect(add('1,2,3,4,5')).toBe(15);
-  });
+//Add test for unknown amount of numbers
+test("returns the sum of any amount of numbers", () => {
+  expect(add("1,2,3,4,5")).toBe(15);
+});
 
-  //Add test for newline delimiter
-  test('handles new lines between numbers', () => {
-    expect(add('1\n2,3')).toBe(6);
-  });
+//Add test for newline delimiter
+test("handles new lines between numbers", () => {
+  expect(add("1\n2,3")).toBe(6);
+});
 
-  //Add test for custom delimiter
-  test('supports custom delimiters', () => {
-    expect(add('//;\n1;2')).toBe(3);
-  });
+//Add test for custom delimiter
+test("supports custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
 
-
-
+//Add test for negative numbers
+test('throws exception for negative numbers', () => {
+  expect(() => add('1,-2,3,-4')).toThrow('negative numbers not allowed -2,-4');
+});
 
--- stringCalculator.js
+++ stringCalculator.js
@@ -16,6 +16,13 @@ function add(numbers) {
   }
   const nums = numbers.split(delimiter).map(num => parseInt(num));
 
+  //Add filter for negative number
+  const negatives = nums.filter(num => num < 0);
+  
+  if (negatives.length > 0) {
+    throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
+  }
+
 
   return nums.reduce((sum, num) => sum + num, 0);
 }
